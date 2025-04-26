86b13cc64242bfb9d585b191b6726467e2155707
Nishka Govil <govil.nishka@berkeley.edu>
2025-04-26 13:43:15 -0700

    added function for visualization

--- yahoo_finance_stock_info_extraction.py
+++ yahoo_finance_stock_info_extraction.py
@@ -1,33 +1,55 @@
-import time
 import yfinance
+import plotly.express as px
 
 
 def stock_ticker_history_for_visualization(stock_ticker):
     stock_ticker = yfinance.Ticker(stock_ticker)
 
     stock_history = stock_ticker.history(period="3mo")
-    stock_history = stock_history[['Open', 'High', 'Low', 'Close']]
+    stock_history = stock_history[['Open', 'High', 'Low', 'Close']].reset_index()
     dividends = stock_ticker.dividends
     info = stock_ticker.info
     market_cap = info.get('marketCap', None)
     p_e_ratio = info.get('trailingPE', None)
 
-    # stock_info_dict = {
-    #     "history": stock_history,
-    #     "dividends": dividends,
-    #     "market cap": market_cap,
-    #     "P/E ratio": p_e_ratio
-    # }
+    stock_info_dict = {
+        "history": stock_history,
+        "dividends": dividends,
+        "market cap": market_cap,
+        "P/E ratio": p_e_ratio
+    }
 
     stock_history['Market Cap'] = market_cap
     stock_history['P/E Ratio'] = p_e_ratio
 
-    stock_history['Dividends'] = stock_history.index.to_series().map(dividends) # NaN for non existing values
+    stock_history['Dividends'] = stock_history['Date'].map(dividends) # NaN for non existing values
 
     stock_history.to_csv(f'{stock_ticker}_visualization_data.csv')
 
-    return 
-    #return stock_info_dict
+    return stock_info_dict
+
+def visualize_stock_history(history_dict, stock_ticker):
+    df = history_dict['history']
+
+    fig = px.line(
+        df,
+        x="Date",
+        y=["High", "Low"],
+        title=f"{stock_ticker} High and Low Prices Over Last 3 Months",
+        labels={"value": "Price (USD)", "Date": "Date", "variable": "Price Type"}
+    )
+
+    fig.update_layout(
+        title_font_size=24,
+        xaxis_title="Date",
+        yaxis_title="Price (USD)",
+        legend_title="Price Type",
+        template="plotly_white",
+        hovermode="x unified"    
+    )
+
+    fig.show()
+
 
 def stock_ticker_history_for_llm(stock_ticker):
     stock_ticker = yfinance.Ticker(stock_ticker)
@@ -76,10 +98,12 @@ def stock_ticker_history_for_llm(stock_ticker):
 
 if __name__ == "__main__":
 
-    ticker = input("Please enter a stock ticker: ")
-    #history = stock_ticker_history_for_visualization(ticker)
+    ticker = input("Please enter a stock ticker: ").upper()
     history = stock_ticker_history_for_llm(ticker)
     if history:
         print(history)
     else:
         print("We could not find information on that stock. Please make sure it is a valid stock")
+
+    # history = stock_ticker_history_for_visualization(ticker)
+    # visualize_stock_history(history, ticker)
