de7119aba6c7b2131e1f79df4dfc6ae257c505af
mirelleg <99463358+mirelleg@users.noreply.github.com>
2025-04-26 10:55:50 -0700

    took care of the case if there was no title or summary to be found for artiles

--- yahoo_finance_scraping.py
+++ yahoo_finance_scraping.py
@@ -6,12 +6,25 @@ def stock_ticker_information(stock_ticker):
     stock_information = yfinance.Ticker(stock_ticker)
 
     stock_news_articles = stock_information.get_news(count=100)
+    if not stock_news_articles:
+        print("There was no news found on this stock")
+        return []
+
     stock_article_titles = []
     stock_article_summaries = []
     stock_article_info = []
     for article in stock_news_articles:
-        title = article['content']['title']
-        summary = article['content']['summary']
+        title = ""
+        summary = ""
+        try:
+            title = article['content']['title']
+        except KeyError:
+            title = "" # in the case where a title cant be found
+    
+        try:
+            summary = article['content']['summary']
+        except KeyError:
+            summary = "" # in the case where a summary cant be found
         
         stock_article_titles.append(title)
         stock_article_summaries.append(summary)
